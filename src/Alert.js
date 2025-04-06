3c0cb324275c6118714e08635392875ff81e8f57
Harmanpreet0870 <harmanpreet0870@gmail.com>
2025-04-06 21:44:41 +0530

    second

--- src/Alert.js
+++ src/Alert.js
@@ -8,9 +8,12 @@ export default function Alert(props) {
 
 
   return (
-    props.alert&&<div className={`alert alert-${props.alert.typ} alert-dismissible fade show`} role="alert">
+    <div style={{height:'50px'}}>
+    {props.alert&&<div className={`alert alert-${props.alert.typ} alert-dismissible fade show`} role="alert">
   <strong> {cap(props.alert.typ)}</strong>:{props.alert.msg}
  
+</div>}
 </div>
   )
 }
+
